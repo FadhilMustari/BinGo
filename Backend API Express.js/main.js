9e8dc8e3b79d23630264f0aabc1a37f4ca17a2fc
M Fadhil Mustari <mfadhilmustari@Fadhils-MacBook-Air.local>
2024-06-01 00:48:26 +0700

    Fixing folder

--- Backend API Express.js/main.js
+++ Backend API Express.js/main.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const userRouter = require('./routes/user.route');
-const todoRouter = require('./routes/todo.route');
 require('./middlewares/passport');
 
 const app = express();
 const { PORT } = process.env;
 
 app.use(express.json());
-app.use('/user', userRouter, todoRouter);
+app.use('/user', userRouter, );
 
 app.listen(PORT, () => {
   console.log(`Server running di port ${PORT}`);
